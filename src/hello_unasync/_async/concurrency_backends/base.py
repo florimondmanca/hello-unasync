0903074cf49ab87cde4906034df738cf1288e220
florimondmanca <florimond.manca@gmail.com>
2019-12-08 11:02:55 +0100

    Document why _async.concurrency_backends.base exists

--- src/hello_unasync/_async/concurrency_backends/base.py
+++ src/hello_unasync/_async/concurrency_backends/base.py
@@ -1,3 +1,10 @@
 class ConcurrencyBackend:
+    """
+    Base class for concurrency backends.
+
+    Needs to be in this directory ('_async/...') so that a sync equivalent is created
+    by `unasync` in the '_sync/...' sub-package at build time.
+    """
+
     async def sleep(self, seconds: float) -> None:
         raise NotImplementedError  # pragma: no cover
