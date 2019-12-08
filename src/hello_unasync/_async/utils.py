481dfe4ce68548647a885d1aa4b138c170c7df30
florimondmanca <florimond.manca@gmail.com>
2019-12-08 14:51:40 +0100

    Improve documentation for is_async_mode()

--- src/hello_unasync/_async/utils.py
+++ src/hello_unasync/_async/utils.py
@@ -1,11 +1,14 @@
 def is_async_mode() -> bool:
-    """Tests if we're in the async part of the code."""
+    """
+    Return True if we are in the '_async' part of the code,
+    or False if we are in the '_sync' part of the code.
+    """
 
     async def f() -> None:
         return None
 
     coro = f()
-    if coro is None:
+    if coro is None:  # `unasync` stripped 'async' from the definition of 'f()'.
         return False
 
     coro.close()  # Prevent unawaited coroutine warning.
